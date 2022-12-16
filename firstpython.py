13cff664ac81d7f60e0e1675cf9bea27cc73e73a
GreenSeeldlessGrapes <120619481+GreenSeeldlessGrapes@users.noreply.github.com>
2022-12-15 19:13:28 -0500

    Update firstpython.py

--- firstpython.py
+++ firstpython.py
@@ -1,5 +1,4 @@
-I_HATE_THE_ANTICHRIST = 777
-if I_HATE_THE_ANTICHRIST > 10:
-    print("I HATE THE ANTI CHRIST " * 7777777)
+x = int(input("how many times do you hate the antichrist? "))
+print("I HATE THE ANTI CHRIST " * x)
 
 
