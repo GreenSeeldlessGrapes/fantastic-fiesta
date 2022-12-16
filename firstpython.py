897c3a905dbfff536edec4b4eca341379137888c
GreenSeeldlessGrapes <120619481+GreenSeeldlessGrapes@users.noreply.github.com>
2022-12-15 22:00:15 -0500

    Update firstpython.py

--- firstpython.py
+++ firstpython.py
@@ -1,4 +1,7 @@
-x = int(input("On a scale of 1 to 10, how much do you hate the antichrist?"))
-print("I HATE THE ANTI CHRIST " * x)
-
-
+while True:
+    try:
+        x = int(input("On a scale of 1 to 10, how much do you hate the antichrist? "))
+        print("I HATE THE ANTI CHRIST " * x)
+        exit()
+    except ValueError:
+        print("I can only interpret hatred in intergers, try again ")
