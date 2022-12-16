b7a26e8b2ba4aaa1253d673770b8e85fbd262475
GreenSeeldlessGrapes <120619481+GreenSeeldlessGrapes@users.noreply.github.com>
2022-12-15 22:49:12 -0500

    Update firstpython.py

--- firstpython.py
+++ firstpython.py
@@ -1,8 +1,12 @@
 while True:
     try:
         x = int(input("On a scale of 1 to 10, how much do you hate the antichrist? "))
-        print("I HATE THE ANTI CHRIST " * x)
-        exit()
+        if x > 0:
+            print("I HATE THE ANTI CHRIST " * x)
+            exit()
+        else:
+            print("YOU DONT HATE THE ANTI CHRIST? WHAT A LOSER!")
+            exit()
     except ValueError:
         print("I can only interpret hatred in intergers, try again ")
     except MemoryError:
